10352f7803bff8901a525da5ddbe38b1eec5aaba
start.vaadin.com <no-reply@vaadin.com>
2024-10-07 15:38:35 +0000

    Generated project

---
+++ src/main/java/com/webapp/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.webapp.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/webapp/application/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.webapp.application.services;
+
+import org.springframework.lang.NonNullApi;
